440c6398139828958191869d575f98c8749ffb29
Chesca <fdjr401@gmail.com>
2025-02-05 11:42:41 +0800

    Add handling of edge cases

--- IntStack.hpp
+++ IntStack.hpp
@@ -5,32 +5,65 @@ using namespace std;
 struct IntStack{
 
 	IntNode* top;
+
+  IntStack() {
+    top = nullptr;
+  }
     
   // remove a node
+  // if this empties the stack, change top to null
+  // if stack is already empty, do not allow pop
   void pop(){
+    if (top == nullptr) {
+      cout << "tried to remove a node but stack is already empty" << endl;
+    }
+    else {
       IntNode* newTop = top->next;
-      delete top;
-      top = newTop;
-      cout << "removed a node" << endl;
+      if (newTop == nullptr) {
+        top = nullptr;
+        cout << "removed a node, stack is now empty" << endl;
+      }
+      else {
+        delete top;
+        top = newTop;
+        cout << "removed a node" << endl;
+      }
+    }
   }
   
   // add a node
+  // if adding to an empty stack, change next to null
   void push(int v) {
-      IntNode* newNode = new IntNode(v);
+    IntNode* newNode = new IntNode(v);
+    if (top == nullptr) {
+      newNode->next = nullptr;
+    }
+    else {
       newNode->next = top;
-      top = newNode;
-      cout << "added a node with data " << v << endl;
+    }
+    top = newNode;
+    cout << "added a node with data " << v << endl;
   }
   
   // show all items
   void peek() {
     cout << "list of data in stack (starting from the top):" << endl;
-    IntNode* currentNode = top;
-    IntNode* nextNode = top->next;
-    while (nextNode != nullptr) {
-      cout << currentNode->data << endl;
-      currentNode = nextNode;
-      nextNode = currentNode->next;
+    if (top == nullptr) {
+      cout << "stack is empty" << endl;
+    }
+    else {
+      IntNode* currentNode = top;
+      IntNode* nextNode = top->next;
+      while (true) {
+        cout << currentNode->data << endl;
+        currentNode = nextNode;
+        if (currentNode == nullptr) {
+          break;
+        }
+        else {
+          nextNode = currentNode->next;
+        }
+      }
     }
   }
 
--- lab3.cpp
+++ lab3.cpp
@@ -3,18 +3,28 @@ using namespace std;
 
 int main( void ) {
   IntStack stack;
+  
+  stack.peek(); // should be empty
 
   stack.push(1);
   stack.push(2);
   stack.push(3);
+  stack.peek(); // should have 3, 2, and 1
 
   stack.pop();
   stack.pop();
   stack.push(4);
+  stack.peek(); // should have 4 and 1
 
-  stack.peek();
   stack.pop();
-  stack.peek();
+  stack.peek(); // should have 1
+
+  stack.pop();
+  stack.pop(); // should not allow pop
+  stack.peek(); // should be empty
+
+  stack.push(1);
+  stack.peek(); // should have 1
 
   return 0;
 }
