19a59472812a3ef914a88292b4889da22c4c92d4
Chesca <fdjr401@gmail.com>
2025-02-24 21:07:18 +0800

    Generate assembly file and delete unneeded files

---
+++ Lab4_AbdonOngkikoReyes_code.s
@@ -0,0 +1,170 @@
+	.file	"Lab4_AbdonOngkikoReyes_code.cpp"
+	.text
+#APP
+	.globl _ZSt21ios_base_library_initv
+#NO_APP
+	.globl	_Z6shlmulii
+	.type	_Z6shlmulii, @function
+_Z6shlmulii:
+.LFB2011:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -20(%rbp)
+	movl	%esi, -24(%rbp)
+	movl	$0, -8(%rbp)
+	movl	$0, -4(%rbp)
+	movl	-20(%rbp), %eax
+	xorl	-24(%rbp), %eax
+	shrl	$31, %eax
+	movzbl	%al, %eax
+	testl	%eax, %eax
+	setne	%al
+	movb	%al, -9(%rbp)
+	movl	-20(%rbp), %eax
+	movl	%eax, %edx
+	negl	%edx
+	cmovns	%edx, %eax
+	movl	%eax, -20(%rbp)
+	movl	-24(%rbp), %eax
+	movl	%eax, %edx
+	negl	%edx
+	cmovns	%edx, %eax
+	movl	%eax, -24(%rbp)
+	jmp	.L2
+.L4:
+	movl	-24(%rbp), %eax
+	andl	$1, %eax
+	testl	%eax, %eax
+	je	.L3
+	movl	-4(%rbp), %eax
+	movl	-20(%rbp), %edx
+	movl	%eax, %ecx
+	sall	%cl, %edx
+	movl	%edx, %eax
+	addl	%eax, -8(%rbp)
+.L3:
+	addl	$1, -4(%rbp)
+	sarl	-24(%rbp)
+.L2:
+	cmpl	$0, -24(%rbp)
+	jne	.L4
+	cmpb	$0, -9(%rbp)
+	je	.L5
+	movl	-8(%rbp), %eax
+	negl	%eax
+	jmp	.L6
+.L5:
+	movl	-8(%rbp), %eax
+.L6:
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2011:
+	.size	_Z6shlmulii, .-_Z6shlmulii
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Multiply two numbers optimally!"
+.LC1:
+	.string	"First number: "
+.LC2:
+	.string	"Last number: "
+.LC3:
+	.string	"Their product is "
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2012:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	subq	$24, %rsp
+	.cfi_offset 3, -24
+	movq	%fs:40, %rax
+	movq	%rax, -24(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	-32(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZNSirsERi@PLT
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	-28(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZNSirsERi@PLT
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rbx
+	movl	-28(%rbp), %edx
+	movl	-32(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	_Z6shlmulii
+	movl	%eax, %esi
+	movq	%rbx, %rdi
+	call	_ZNSolsEi@PLT
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+	movl	$0, %eax
+	movq	-24(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L9
+	call	__stack_chk_fail@PLT
+.L9:
+	movq	-8(%rbp), %rbx
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2012:
+	.size	main, .-main
+	.section	.rodata
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE:
+	.byte	1
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedImEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedImEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedImEE:
+	.byte	1
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE:
+	.byte	1
+	.ident	"GCC: (GNU) 14.2.1 20250207"
+	.section	.note.GNU-stack,"",@progbits
