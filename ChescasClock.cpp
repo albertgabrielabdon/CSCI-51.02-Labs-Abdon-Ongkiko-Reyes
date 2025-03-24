72ed99a2528f72ef33eba6e556f47ba1547b0403
Chesca <fdjr401@gmail.com>
2025-03-24 12:04:39 +0800

    Add Chesca's Lab6 file

---
+++ ChescasClock.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+  pid_t forkRet = fork();
+
+  if (forkRet == 0) {
+    if (execl("/usr/bin/xclock", "myXclock", NULL) == -1) {
+      cout << "Could not open or find the program." << endl;
+    }
+  }
+
+  time_t timestamp;
+  tm * datetime;
+  char output[50];
+
+  int i = 0;
+
+  while (true) {
+    time(&timestamp);
+    datetime = localtime(&timestamp);
+    strftime(output, 50, "%F %T", datetime); // [yyyy-mm-dd] hh:mm:ss
+
+    cout << output << endl;
+    i++;
+
+    if (i == 3) {
+      cout << "\"This program has gone on for far too long.\"" << endl;
+      cout << "\"Type Ctrl+C to abort this timer application.\"" << endl;
+      i = 0;
+    }
+
+    sleep(10);
+  }
+}
